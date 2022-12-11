1b7cb89728faa6f9028e4690d189ca68a9bf45cf
Zajancz <105783363+Zajancz@users.noreply.github.com>
2022-12-11 17:56:17 +0100

    Added a function for inverting text

--- src/main/java/pl/put/poznan/transformer/logic/InversionTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/InversionTextTransformer.java
@@ -5,4 +5,16 @@ public class InversionTextTransformer extends DecoratedTextTransformer{
     public InversionTextTransformer(TextTransformer textTransformer) {
         super(textTransformer);
     }
+
+    public static String inversion(final String input) {
+        final char[] charsInput = input.toCharArray();
+        final char[] charsOutput = new char[charsInput.length];
+        for (int i = 0; i < charsInput.length; i++) {
+            final char c = charsInput[i];
+            //final char output = Character.isUpperCase(c) ? Character.toLowerCase(c) : Character.toUpperCase(c);
+            charsOutput[charsInput.length - i - 1] = c;
+        }
+        return new String(charsOutput);
+    }
+
 }
