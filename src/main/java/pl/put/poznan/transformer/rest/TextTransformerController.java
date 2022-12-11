60e3ce7f966cf0ffe8a6f3a332663679bd6c003c
Zajancz <105783363+Zajancz@users.noreply.github.com>
2022-12-11 18:02:36 +0100

    Made paths for calling certain functions

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -2,6 +2,8 @@ package pl.put.poznan.transformer.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.EscapeCharactersTextTransformer;
+import pl.put.poznan.transformer.logic.InversionTextTransformer;
 import pl.put.poznan.transformer.logic.TextTransformer;
 
 import java.util.Arrays;
@@ -13,9 +15,9 @@ public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+    @RequestMapping(value = "/transforms/inverse",method = RequestMethod.GET, produces = "application/json")
+    public String inverse(@PathVariable String text,
+                              String[] transforms) {
 
         // log the parameters
         logger.debug(text);
@@ -23,8 +25,28 @@ public class TextTransformerController {
 
         // perform the transformation, you should run your logic here, below is just a silly example
         TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+
+        String test = Arrays.toString(transforms);
+
+        return InversionTextTransformer.inversion(text);
     }
+
+    @RequestMapping(value = "/transforms/escape",method = RequestMethod.GET, produces = "application/json")
+    public String escape(@PathVariable String text,
+                          String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        TextTransformer transformer = new TextTransformer(transforms);
+
+        String test = Arrays.toString(transforms);
+
+        return EscapeCharactersTextTransformer.Latex(text);
+    }
+
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public String post(@PathVariable String text,
