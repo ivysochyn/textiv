ff920a4f198e328a606ca6a03ee0e9c6844ab0b6
Zajancz <105783363+Zajancz@users.noreply.github.com>
2022-12-11 17:57:33 +0100

    Added a function for escaping text into Latex form

--- src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
@@ -1,7 +1,16 @@
 package pl.put.poznan.transformer.logic;
 
+
 public class EscapeCharactersTextTransformer extends DecoratedTextTransformer{
     public EscapeCharactersTextTransformer(TextTransformer textTransformer) {
         super(textTransformer);
     }
+
+
+    public static String Latex(String input) {
+        String result = input.replaceAll("&", "/&");
+        String FinalResult = result.replace("$", "/$");
+        return FinalResult;
+    }
+
 }
