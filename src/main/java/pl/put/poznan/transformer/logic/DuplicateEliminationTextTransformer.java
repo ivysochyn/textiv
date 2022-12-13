6c0ea973093df37eba8859e8a306ec4062227bb8
Illia Vysochyn <bitfun6@gmail.com>
2022-12-13 17:26:34 +0100

    [#8] src: DuplicateEliminationTextTransformer: Add initial implementation

--- src/main/java/pl/put/poznan/transformer/logic/DuplicateEliminationTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/DuplicateEliminationTextTransformer.java
@@ -4,4 +4,16 @@ public class DuplicateEliminationTextTransformer extends DecoratedTextTransforme
     public DuplicateEliminationTextTransformer(TextTransformer textTransformer) {
         super(textTransformer);
     }
+
+    @Override
+    public String transform(String text) {
+        String[] words = text.split(" ");
+        String result = "";
+        for (int i = 0; i < words.length; i++) {
+            if (i == 0 || !words[i].equals(words[i - 1])) {
+                result += words[i] + " ";
+            }
+        }
+        return textTransformer.transform(result);
+    }
 }
