7a14bbb1ca5cb62a5ab26360009a630eb5e3750d
unbreaking-V <uladzislau.abeziuk@student.put.poznan.pl>
2022-12-13 13:39:18 +0100

    [#10] src: main: java: CaseTextTransformer: Added case change functionality
    A set of three transformations has been added:
    *UpperCaseTransform
    *LowerCaseTransform
    *CapitalizeCaseTransform

--- src/main/java/pl/put/poznan/transformer/logic/CaseTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/CaseTextTransformer.java
@@ -1,19 +1,42 @@
 package pl.put.poznan.transformer.logic;
 
-import org.springframework.util.StringUtils;
-import pl.put.poznan.transformer.logic.TextTransformer;
-
 public class CaseTextTransformer extends DecoratedTextTransformer {
-  String state;
+    String state;
+   
+    @Override
+    public String transform(String text) {
+        switch (state) {
+            case "upper":
+                return UpperCaseTransform(textTransformer.transform(text));
+            case "lower":
+                return LowerCaseTransform(textTransformer.transform(text));
+            case "capitalize":
+                return CapitalizeCaseTransform(textTransformer.transform(text));
+        }
+        return text;
+    }
+    public CaseTextTransformer(TextTransformer textTransformer, String state) {
+        super(textTransformer);
+        this.state=state;
 
-  /**
-   * Initialize the decorator with the state of the transformer.
-   *
-   * @param textTransformer TextTransformer
-   * @param state a string that represents whether the text should be uppercased or lowercased
-   */
-  public CaseTextTransformer(TextTransformer textTransformer, String state) {
-    super(textTransformer);
-    this.state = state;
-  }
+    }
+
+    public static String UpperCaseTransform(String strInLowerCase){
+        return strInLowerCase.toUpperCase();
+    }
+
+    public static String LowerCaseTransform(String strInUpperCase){
+        return  strInUpperCase.toLowerCase();
+    }
+
+    public static String CapitalizeCaseTransform(String strInCase){
+        StringBuffer res = new StringBuffer();
+        String[] subStr = strInCase.split(" ");
+        for (String s : subStr) {
+            String cap = s.substring(0, 1).toUpperCase();
+            res.insert(res.length(), cap + s.substring(1) + " ");
+        }
+        String result = String.valueOf(res);
+        return result;
+    }
 }
