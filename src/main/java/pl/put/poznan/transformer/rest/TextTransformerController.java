71079ee6f94ca7fb40d5ea3f608ca14eb004ccc6
Mikolaj Zaleski <mikolajzaleski@outlook.com>
2022-12-13 13:41:45 +0100

    []

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,12 +1,22 @@
 package pl.put.poznan.transformer.rest;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.EscapeCharactersTextTransformer;
 import pl.put.poznan.transformer.logic.InversionTextTransformer;
 import pl.put.poznan.transformer.logic.TextTransformer;
+import pl.put.poznan.transformer.logic.TextTransformerCreator;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 
 @RestController
@@ -48,17 +58,22 @@ public class TextTransformerController {
     }
 
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
+@PostMapping(value="/transform")
+public String post(@PathVariable String text,@RequestBody String json) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode jsonObject = mapper.readTree(json);
+   JsonNode transformsNode= jsonObject.get("transforms");
+   String transformsString=transformsNode.toString();
+   List <String> transforms=new ArrayList<>();
+     transforms=mapper.readValue(transformsString,transforms.getClass());
         // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+      //  TextTransformer transformer = new TextTransformer(transforms);
+       // return transformer.transform(text);
+    String[] transformsArray=new String[transforms.size()];
+    transforms.toArray(transformsArray);
+    TextTransformer textTransformer=new TextTransformer(transformsArray);
+     textTransformer= TextTransformerCreator.createTextTransformer(textTransformer);
+    return textTransformer.transform(text);
     }
 
 
