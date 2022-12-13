dc3c6ab26929f08221f31e4ae56b65708c69861e
Illia Vysochyn <bitfun6@gmail.com>
2022-12-13 18:07:56 +0100

    [#8] src: DuplicateEliminationTextTransformer: Add documentation
    Fixes edge cases with spaces

--- src/main/java/pl/put/poznan/transformer/logic/DuplicateEliminationTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/DuplicateEliminationTextTransformer.java
@@ -5,15 +5,25 @@ public class DuplicateEliminationTextTransformer extends DecoratedTextTransforme
         super(textTransformer);
     }
 
+    /**
+     * <p>
+     * This method removes next to each other duplicate words from text.
+     * </p>
+     *
+     * @param text A String object to remove duplications from.
+     * @return Text without duplicate words next to each other.
+     */
     @Override
     public String transform(String text) {
-        String[] words = text.split(" ");
+        String[] words = text.split("\\s+");
         String result = "";
+
         for (int i = 0; i < words.length; i++) {
             if (i == 0 || !words[i].equals(words[i - 1])) {
                 result += words[i] + " ";
             }
         }
+        result = result.trim();
         return textTransformer.transform(result);
     }
 }
