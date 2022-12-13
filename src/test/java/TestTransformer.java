306ba8093760bb333371824257223eb7457856a7
Illia Vysochyn <bitfun6@gmail.com>
2022-12-13 12:40:02 +0100

    src: test: Fix tests

--- src/test/java/TestTransformer.java
+++ src/test/java/TestTransformer.java
@@ -1,17 +1,18 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import pl.put.poznan.transformer.logic.TextTransformer;
 import pl.put.poznan.transformer.logic.TextTransformerCreator;
 
-public class testTestTransformer {
+public class TestTransformer {
 
 
     @Test
-    void testTextTransformerWrapping(){
+    void TestTextTransformerWrapping(){
         TextTransformer textTransformer;
         textTransformer=new TextTransformer(new String[]{"capitalize","collapsing","num2word"});
 
         TextTransformer tex= TextTransformerCreator.createTextTransformer(textTransformer);
-        System.out.println( tex.transform("S"));
+        assertEquals(1, 2);
     }
 }
