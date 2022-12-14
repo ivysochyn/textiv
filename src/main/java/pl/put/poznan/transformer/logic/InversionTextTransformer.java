fcf257fdb7396848d079cc939b1ca1d85ab507e9
Artiom Zajanczkowski <seledka.artem123@gmail.com>
2022-12-14 17:36:59 +0100

    [#16] src: main: java: InversionTextTransformer: Modify the text inverting function
    Adds documentation for the text invert function, as well as for the transformation method

--- src/main/java/pl/put/poznan/transformer/logic/InversionTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/InversionTextTransformer.java
@@ -6,13 +6,29 @@ public class InversionTextTransformer extends DecoratedTextTransformer {
     super(textTransformer);
   }
 
+  /**
+   * This function passes the value of the inverted text further along the layers
+   *
+   * @param text We pass to this parameter the text that we want to invert
+   * @return Returns the inverted text
+   */
+
+  @Override
+  public String transform(String text) {
+    return inversion(textTransformer.transform(text));
+  }
+
+  /**
+   * This function is used to invert the text while preserving the case
+   * @param input We pass to this parameter the text that we want to invert
+   * @return Returns the inverted text
+   */
+
   public static String inversion(final String input) {
     final char[] charsInput = input.toCharArray();
     final char[] charsOutput = new char[charsInput.length];
     for (int i = 0; i < charsInput.length; i++) {
       final char c = charsInput[i];
-      // final char output = Character.isUpperCase(c) ? Character.toLowerCase(c) :
-      // Character.toUpperCase(c);
       charsOutput[charsInput.length - i - 1] = c;
     }
     return new String(charsOutput);
