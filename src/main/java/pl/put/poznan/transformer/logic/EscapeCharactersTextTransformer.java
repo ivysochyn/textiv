bd8eab926843390de80f51ce21eb39de309c9aa7
Zajancz <seledka.artem123@gmail.com>
2022-12-14 18:44:23 +0100

    [#17] src: main: java: EscapeCharactersTextTransformer: Adding documentation to two methods
    Documentation for the Latex function used to change special characters in a text we provide to Latex understandable form. Also documentation for the transformation method

--- src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
@@ -5,10 +5,24 @@ public class EscapeCharactersTextTransformer extends DecoratedTextTransformer {
     super(textTransformer);
   }
 
+  /**
+   * This function passes the value of the changed text further along the layers
+   *
+   * @param text We pass to this parameter the text, where we want to change symbols to Latex form
+   * @return Returns the changed text
+   */
+
+
   @Override
   public String transform(String text) {
     return Latex(textTransformer.transform(text));
   }
+
+  /**
+   * This function is used to change special characters into a form that is understandable to the Latex program
+   * @param input We pass to this parameter the text, in which we want to change special characters
+   * @return Returns the text with changed special characters that is understandable to the Latex program
+   */
 
   public static String Latex(String input) {
     String result = input.replaceAll("&", "/&");
