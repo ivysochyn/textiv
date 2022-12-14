9d8d6a68c1ffdbca03d8a00481c36c7be412b2e8
Zajancz <seledka.artem123@gmail.com>
2022-12-14 18:38:46 +0100

    [#17] src: main: java: EscapeCharactersTextTransformer: Adding the transform method to a class
    For the correct operations of business logic

--- src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
@@ -5,6 +5,11 @@ public class EscapeCharactersTextTransformer extends DecoratedTextTransformer {
     super(textTransformer);
   }
 
+  @Override
+  public String transform(String text) {
+    return Latex(textTransformer.transform(text));
+  }
+
   public static String Latex(String input) {
     String result = input.replaceAll("&", "/&");
     String FinalResult = result.replace("$", "/$");
