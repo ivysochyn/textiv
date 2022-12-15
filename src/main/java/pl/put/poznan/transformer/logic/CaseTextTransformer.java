7986c6db3c1a75894a6216631ea9a18c388b4f48
unbreaking-V <uladzislau.abeziuk@student.put.poznan.pl>
2022-12-15 21:26:19 +0100

    [#10] src: main: java: CaseTextTransformer: Added javadoc to CaseTextTransformer function

--- src/main/java/pl/put/poznan/transformer/logic/CaseTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/CaseTextTransformer.java
@@ -4,6 +4,14 @@ public class CaseTextTransformer extends DecoratedTextTransformer {
     String state;
    
     @Override
+   /**
+      * <p>
+      * This method exists to select the method used in the class based on the state variable .  
+      * </p>
+      *
+      * @param text  String object to transform  
+      * @return Converted text , when selecting the method
+    */
     public String transform(String text) {
         switch (state) {
             case "upper":
@@ -15,19 +23,45 @@ public class CaseTextTransformer extends DecoratedTextTransformer {
         }
         return text;
     }
+
     public CaseTextTransformer(TextTransformer textTransformer, String state) {
         super(textTransformer);
         this.state=state;
-
     }
 
+    /**
+     * <p>
+     * This method changes all the letters in the text to uppercase  
+     * </p>
+     *
+     * @param strInLowerCase  String object to change all letters to uppercase  
+     * @return Text in which all letters are uppercase
+     */
     public static String UpperCaseTransform(String strInLowerCase){
         return strInLowerCase.toUpperCase();
     }
 
+
+      /**
+      * <p>
+      * This method changes all letters in the text to lowercase 
+      * </p>
+      *
+      * @param strInUpperCase  String object to change all letters to lowercase
+      * @return Text in which all letters are lowercase.
+      */
     public static String LowerCaseTransform(String strInUpperCase){
         return  strInUpperCase.toLowerCase();
     }
+
+     /**
+       * <p>
+       * This method changes first letter in each word to uppercase 
+       * </p>
+       *
+       * @param strInCase  String object to change first letter in each word to uppercase 
+       * @return Text in which first letter in each word to uppercase 
+       */
 
     public static String CapitalizeCaseTransform(String strInCase){
         StringBuffer res = new StringBuffer();
