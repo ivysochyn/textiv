ab759e5b2a32def9dc90777837ba79ffab33f0a4
Illia Vysochyn <bitfun6@gmail.com>
2022-12-17 19:07:24 +0100

    [#31] src: main: Add not working base for web GUI
    On page loading because 'th:field' can't be parsed the error is dropped.

--- src/main/java/pl/put/poznan/transformer/app/index/TextTransformerWeb.java
+++ src/main/java/pl/put/poznan/transformer/app/index/TextTransformerWeb.java
@@ -1,13 +1,24 @@
 package pl.put.poznan.transformer.app.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class TextTransformerWeb {
 
   @GetMapping("/")
-  String index() {
+  public String textForm(Model model) {
+    InputText inputText = new InputText();
+    model.addAttribute("inputText", inputText);
     return "index";
+  }
+
+  @PostMapping("/")
+  public String textSubmit(@ModelAttribute InputText text, Model model) {
+    model.addAttribute("inputText", text);
+    return "result";
   }
 }
