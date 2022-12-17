8d53c73ddb42d8d5ed63a08b99721be28e3487d1
Zajancz <seledka.artem123@gmail.com>
2022-12-17 12:38:42 +0100

    [#13] src: main: java: InversionTextTransformer: Added logging on INFO and DEBUG levels to InversionTT
    logging input on info level and output on debug level

--- src/main/java/pl/put/poznan/transformer/logic/InversionTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/InversionTextTransformer.java
@@ -1,6 +1,11 @@
 package pl.put.poznan.transformer.logic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.put.poznan.transformer.rest.TextTransformerController;
 
 public class InversionTextTransformer extends DecoratedTextTransformer {
+
+  private static final Logger logger = LoggerFactory.getLogger(InversionTextTransformer.class);
 
   public InversionTextTransformer(TextTransformer textTransformer) {
     super(textTransformer);
@@ -23,12 +28,14 @@ public class InversionTextTransformer extends DecoratedTextTransformer {
    * @return Returns the inverted text
    */
   public static String inversion(final String input) {
+    logger.info(input);
     final char[] charsInput = input.toCharArray();
     final char[] charsOutput = new char[charsInput.length];
     for (int i = 0; i < charsInput.length; i++) {
       final char c = charsInput[i];
       charsOutput[charsInput.length - i - 1] = c;
     }
+    logger.debug(new String(charsOutput));
     return new String(charsOutput);
   }
 }
