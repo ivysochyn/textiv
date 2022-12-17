e9abe6903fb9efc71fd4a3592a6ab328f1dfc470
Zajancz <seledka.artem123@gmail.com>
2022-12-17 10:29:55 +0100

    [#17] src: test: java: EscapeCharactersTextTransformerTest: Corrected tests for new function

--- src/test/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformerTest.java
@@ -6,28 +6,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EscapeCharactersTextTransformerTest {
-    private TextTransformer textTransformer;
+  private TextTransformer textTransformer;
 
-    @BeforeEach
-    public void createTransformer() {
-        textTransformer = new TextTransformer(new String[] {"escape"});
-        textTransformer = TextTransformerCreator.createTextTransformer(textTransformer);
-    }
+  @BeforeEach
+  public void createTransformer() {
+    textTransformer = new TextTransformer(new String[] {"escape"});
+    textTransformer = TextTransformerCreator.createTextTransformer(textTransformer);
+  }
 
-    @Test
-    public void testTextAndToLatex() {
-        String text = "John Smith & Sons";
-        String expected = "John Smith /& Sons";
-        String actual = textTransformer.transform(text);
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void testTextAndToLatex() {
+    String text = "John Smith & Sons";
+    String expected = "John Smith \\& Sons";
+    String actual = textTransformer.transform(text);
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testTextDollarToLatex() {
-        String text = "John Smith $ Sons";
-        String expected = "John Smith /$ Sons";
-        String actual = textTransformer.transform(text);
-        assertEquals(expected, actual);
-    }
-
+  @Test
+  public void testTextDollarToLatex() {
+    String text = "John Smith $ Sons";
+    String expected = "John Smith \\$ Sons";
+    String actual = textTransformer.transform(text);
+    assertEquals(expected, actual);
+  }
 }
