959f5ed9a601de8cd96fd94ae93c5bdf6156be8e
Illia Vysochyn <bitfun6@gmail.com>
2022-12-17 19:07:24 +0100

    [#31] src: main: Add not working base for web GUI
    On page loading because 'th:field' can't be parsed the error is dropped.

---
+++ src/main/java/pl/put/poznan/transformer/app/index/InputText.java
@@ -0,0 +1,14 @@
+package pl.put.poznan.transformer.app.index;
+
+public class InputText {
+
+  private String content;
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+}
