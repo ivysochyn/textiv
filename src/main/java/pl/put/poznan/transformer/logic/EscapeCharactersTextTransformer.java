e98ec61a0dbf5b9c4b8f902d25944efed0045877
Illia Vysochyn <bitfun6@gmail.com>
2022-12-20 00:05:22 +0100

    [#31] src: main: java: EscapeCharactersTextTransformer: Fix looger class

--- src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/EscapeCharactersTextTransformer.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class EscapeCharactersTextTransformer extends DecoratedTextTransformer {
 
-  private static final Logger logger = LoggerFactory.getLogger(InversionTextTransformer.class);
+  private static final Logger logger = LoggerFactory.getLogger(EscapeCharactersTextTransformer.class);
 
   public EscapeCharactersTextTransformer(ITextTransformer textTransformer) {
     super(textTransformer);
