84a24c190d787edbfb7ab963bf2f3515b495ac00
Illia Vysochyn <bitfun6@gmail.com>
2022-12-19 22:08:08 +0100

    [#12] src: test: java: AbbreviationTextTransformerTest: Fix indentation

--- src/test/java/pl/put/poznan/transformer/logic/AbbreviationTextTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/AbbreviationTextTransformerTest.java
@@ -5,17 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class AbbreviationTextTransformerTest {
   private TextTransformer textTransformerCollapse;
   private TextTransformer textTransformerExpand;
+
   @BeforeEach
-  public void createTransformer(){
-    textTransformerCollapse=new TextTransformer(new String[]{"collapsing"});
-    textTransformerExpand=new TextTransformer(new String[]{"expanding"});
-    textTransformerCollapse=TextTransformerCreator.createTextTransformer(textTransformerCollapse);
-    textTransformerExpand=TextTransformerCreator.createTextTransformer(textTransformerExpand);
+  public void createTransformer() {
+    textTransformerCollapse = new TextTransformer(new String[] {"collapsing"});
+    textTransformerExpand = new TextTransformer(new String[] {"expanding"});
+    textTransformerCollapse = TextTransformerCreator.createTextTransformer(textTransformerCollapse);
+    textTransformerExpand = TextTransformerCreator.createTextTransformer(textTransformerExpand);
   }
+
   @Test
   void testOneWordExpanding() {
     String input = "Dr John Smith";
@@ -23,29 +24,30 @@ class AbbreviationTextTransformerTest {
     String actual = textTransformerExpand.transform(input);
     assertEquals(expected, actual);
   }
-  @Test
-  void testOneWordCollapsing(){
-    String expected="Dr John Smith";
-    String input="Doctor John Smith";
-    String actual=textTransformerCollapse.transform(input);
-    assertEquals(expected, actual);
 
+  @Test
+  void testOneWordCollapsing() {
+    String expected = "Dr John Smith";
+    String input = "Doctor John Smith";
+    String actual = textTransformerCollapse.transform(input);
+    assertEquals(expected, actual);
   }
+
   @Test
-  void multipleWordCollapsing(){
+  void multipleWordCollapsing() {
 
-    String expected="Dr John Smith is working on a new project concerning electronics etc.";
-    String input="Doctor John Smith is working on a new project concerning electronics and so on";
-    String actual=textTransformerCollapse.transform(input);
+    String expected = "Dr John Smith is working on a new project concerning electronics etc.";
+    String input = "Doctor John Smith is working on a new project concerning electronics and so on";
+    String actual = textTransformerCollapse.transform(input);
     assertEquals(expected, actual);
-
   }
-  @Test
-  void multipleWordExpanding(){
-    String input="Dr John Smith is working on a new project concerning electronics etc.";
-    String expected="Doctor John Smith is working on a new project concerning electronics and so on";
-    String actual=textTransformerExpand.transform(input);
-    assertEquals(expected, actual);
 
+  @Test
+  void multipleWordExpanding() {
+    String input = "Dr John Smith is working on a new project concerning electronics etc.";
+    String expected =
+        "Doctor John Smith is working on a new project concerning electronics and so on";
+    String actual = textTransformerExpand.transform(input);
+    assertEquals(expected, actual);
   }
 }
