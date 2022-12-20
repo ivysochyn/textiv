75eec62e5f4e078c771895bdebbe3cbbaf60da04
Illia Vysochyn <bitfun6@gmail.com>
2022-12-20 11:03:46 +0100

    [#30] src: main: java: TextTransformerWeb: Refactor code

--- src/main/java/pl/put/poznan/transformer/app/index/TextTransformerWeb.java
+++ src/main/java/pl/put/poznan/transformer/app/index/TextTransformerWeb.java
@@ -1,16 +1,13 @@
 package pl.put.poznan.transformer.app.index;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class TextTransformerWeb {
 
   @GetMapping("/")
-  public String textForm(Model model) {
+  public String index() {
     return "index";
   }
 }
